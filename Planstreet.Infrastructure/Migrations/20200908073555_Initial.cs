c86145af4936e31f6add53d018bed507ea978fd1
Raul Araújo <raulluiz_araujo_12@hotmail.com>
2020-09-08 04:37:48 -0300

    Construct

---
+++ Planstreet.Infrastructure/Migrations/20200908073555_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Planstreet.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DefaultFolder",
+                columns: table => new
+                {
+                    DefaultFolderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Caption = table.Column<string>(maxLength: 100, nullable: true),
+                    CreationTime = table.Column<DateTime>(nullable: true),
+                    UpdationTime = table.Column<DateTime>(nullable: true),
+                    Creator = table.Column<int>(nullable: true),
+                    Updater = table.Column<int>(nullable: true),
+                    RemoteHost = table.Column<string>(maxLength: 50, nullable: true),
+                    Status = table.Column<byte>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DefaultFolder", x => x.DefaultFolderId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DefaultFolder");
+        }
+    }
+}
